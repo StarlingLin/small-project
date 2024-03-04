d34c4c7f65c114faa49bb0627517d680f6eb19b4
椋鸟Starling <2222040903@qq.com>
2024-03-04 16:15:16 +0800

    HiEasyX引入完毕

--- 程序设计小组作业/程序设计小组作业/test.cpp
+++ 程序设计小组作业/程序设计小组作业/test.cpp
@@ -1,13 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <graphics.h>	// EasyX
+#include "HiEasyX.h"	// HiEasyX
 #include "List.h"
 
 #define CH (char*)
 
 int main()
 {
-
-
+	initgraph(800, 600);
+	BEGIN_TASK();
+	END_TASK();
+	REDRAW_WINDOW();
+	getmessage(EM_KEY);
+	closegraph();
 	return EXIT_SUCCESS;
 }
